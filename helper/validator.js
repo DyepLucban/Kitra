6e8f02c906bf49ab7b2c83d7c4c9708152c4cb18
Jeffrey Lucban <lucbanjep@gmail.com>
2023-10-21 11:43:44 +0800

    update validator

--- helper/validator.js
+++ helper/validator.js
@@ -9,8 +9,8 @@ const inputValidator = [
   body('distance', 'Distance must not empty').notEmpty(),
   body('distance', 'Distance must be a whole number').isInt(),
   body('distance', 'Distance must be 1 or 10 only').custom(value => value === 1 || value === 10),
-  body('price_value', 'Price value must be a number').isInt(),
-  body('price_value', 'Price value must be whole number only').custom(value => typeof value === 'number').isInt(),
+  body('price_value', 'Price value must be a number').optional({ falsy: true }).isInt(),
+  body('price_value', 'Price value must be whole number only').optional({ falsy: true }).custom(value => typeof value === 'number').isInt(),
   body('price_value', 'Price value must be minimum by 10').optional({ falsy: true }).isInt({ min: 10 }),
   body('price_value', 'Price value must be maximum by 30').optional({ falsy: true }).isInt({ max: 30 }),
 ]
